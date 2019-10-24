4d59a3f23f575922c9ac281c80ac62153f9a63de
Elske <e.muijderman@gmail.com>
2019-10-24 21:17:27 +0200

    Updated the main method, so the user can choose the interval

--- GuessingGame.java
+++ GuessingGame.java
@@ -4,8 +4,14 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 
-        Guesser guesser = new Guesser(0, 1000);
+		int min = 0;
+		int max = 0;
+		min = Integer.parseInt(args[0]);
+		max = Integer.parseInt(args[1]);
+
+		Guesser guesser = new Guesser(min, max);
 		guesser.start();
+
 	}
 
 }
