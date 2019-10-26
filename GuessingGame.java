e45a683e8fe02201de91d0cacb9b5c3539379c5c
Elske <e.muijderman@gmail.com>
2019-10-26 21:14:53 +0200

    Changed if sats so it checks too many / too little arguments. Added Try/Catch for parsing

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,28 +1,33 @@
-
-
 public class GuessingGame {
 
 	public static void main(String[] args) {
 
+		if (args.length == 0 || args.length < 2 || args.length > 2) {
+                System.out.println("You must provide two arguments");
+                usage();
+                System.exit(1);
+                }
+
+		
 		int min = 0;
 		int max = 0;
-
-		if (args.length < 1 || args.length > 1) {
-		usage();
-		System.exit(1);
-		}
-		else {
+		try{
 		min = Integer.parseInt(args[0]);
                 max = Integer.parseInt(args[1]);
-                Guesser guesser = new Guesser(min, max);
-                guesser.start();
+                }catch(NumberFormatException nfe){
+		System.out.println("The arguments must be numbers");
+		usage();
+		System.exit(2);
 		}
 
-}
+		Guesser guesser = new Guesser(min, max);
+                guesser.start();
+
+	}
 private static void usage() {
-System.out.println("Usage: java GuessingGame low high" +
- "\nwhere low is an integer for the lower bound" + 
-"\nand high is an integer for the upper bound" + 
-"\nof the interval for the numbers of the GuessingGame");
-}
+	System.out.println("Usage: java GuessingGame low high" +
+	 "\nwhere low is an integer for the lower bound" + 
+	"\nand high is an integer for the upper bound" + 
+	"\nof the interval for the numbers of the GuessingGame");
+	}
 }
