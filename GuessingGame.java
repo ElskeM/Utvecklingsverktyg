afe43596da24bb48912a88a5de62e23b865f30d3
Elske <e.muijderman@gmail.com>
2019-10-26 20:11:42 +0200

    Added a If / Else to GuessingGame, in case user doesn't enter arguments for min/max

--- GuessingGame.java
+++ GuessingGame.java
@@ -6,11 +6,19 @@ public class GuessingGame {
 
 		int min = 0;
 		int max = 0;
-		min = Integer.parseInt(args[0]);
-		max = Integer.parseInt(args[1]);
 
+		if (args.length < 1 || args.length > 1) {
+		min = 0;
+		max = 1000;
 		Guesser guesser = new Guesser(min, max);
-		guesser.start();
+                guesser.start();
+		}
+		else {
+		min = Integer.parseInt(args[0]);
+                max = Integer.parseInt(args[1]);
+                Guesser guesser = new Guesser(min, max);
+                guesser.start();
+		}
 
 	}
 
